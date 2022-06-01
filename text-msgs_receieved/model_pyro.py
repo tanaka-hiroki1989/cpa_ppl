ae9e835b3a29737b716ec607e6742b5c9362ab28
Hiroki Tanaka <rokitaniplus1@gmail.com>
2022-06-01 23:07:56 +0900

    modified model and guide

--- text-msgs_receieved/model_pyro.py
+++ text-msgs_receieved/model_pyro.py
@@ -1,29 +1,31 @@
 import torch
 import pyro
 import pyro.distributions as dist
-
+import pyro.distributions.constraints as constraints
 
 def model(data):
-    #hyper parameter
+    N = data.size(0)
     alpha = 1.0 / data.mean()
-    
-    lambda_1 = pyro.sample("lambda_1", dist.Exponential(alpha))
-    lambda_2 = pyro.sample("lambda_2", dist.Exponential(alpha))
-    
-    tau = pyro.sample("tau", dist.Uniform(0, 1))
-    lambda1_size = (tau * data.size(0) + 1).long()
-    lambda2_size = data.size(0) - lambda1_size
-    lambda_ = torch.cat([lambda_1.expand((lambda1_size,)),
-                         lambda_2.expand((lambda2_size,))])
+    lambda1 = pyro.sample("lambda1", dist.Exponential(alpha))
+    lambda2 = pyro.sample("lambda2", dist.Exponential(alpha))
+    pi = torch.tensor([1.0]*N)
+    tau = pyro.sample("tau", dist.Categorical(torch.softmax(pi,0,dtype=torch.double)))
+    lambda1_size = int(tau)
+    lambda2_size = int(N)-lambda1_size
+    lambda_ = torch.cat([lambda1.expand((lambda1_size,)),
+                         lambda2.expand((lambda2_size,))])
 
     with pyro.plate("data", data.size(0)):
         pyro.sample("obs", dist.Poisson(lambda_), obs=data)
 
 def guide(data):
-    alpha_1 = pyro.param('alpha_1', lambda: torch.tensor(0.))
-    alpha_2 = pyro.param('alpha_2', lambda: torch.tensor(1.))
-    theta = pyro.param('theta', lambda: torch.randn(70))
-    lambda_1 = pyro.sample("lambda_1", dist.Exponential(alpha_1))
-    lambda_2 = pyro.sample("lambda_2", dist.Exponential(alpha_2))
-    tau = pyro.sample("tau", dist.Categorical(torch.softmax(theta)))
-    return {"lambda_1": lambda_1, "lambda_2": lambda_2, "tau": tau}
+    N = data.size(0)
+    alpha1 = pyro.param('alpha1', lambda: torch.tensor(10.0,dtype=torch.double),
+        constraint=constraints.positive)
+    alpha2 = pyro.param('alpha2', lambda: torch.tensor(10.0,dtype=torch.double),
+        constraint=constraints.positive)
+    pi = pyro.param('pi',lambda: torch.tensor([1.0]*N))
+    lambda1 = pyro.sample("lambda1", dist.Exponential(alpha1))
+    lambda2 = pyro.sample("lambda2", dist.Exponential(alpha2))
+    tau = pyro.sample("tau", dist.Categorical(torch.softmax(pi,0,torch.double)))
+    return {"lambda1": lambda1, "lambda2": lambda2, "tau": tau}
